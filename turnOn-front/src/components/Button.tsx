5055d3611185c236703546c0d344f8f7ae8702c5
Angelo <ancricoma_angelo@hotmail.com>
2024-11-21 17:15:40 -0500

    added config

--- turnOn-front/src/components/Button.tsx
+++ turnOn-front/src/components/Button.tsx
@@ -1,14 +1,13 @@
-
 type ButtonProps = {
-    label: string;
-    onClick?: () => void;
-    className?: string;
-}
+  label: string;
+  onClick?: () => void;
+  className?: string;
+};
 
 export const Button = ({ label, onClick, className }: ButtonProps) => {
-    return (
-        <button onClick={onClick} className={className}>
-            {label}
-        </button>
-    ) 
-}
+  return (
+    <button onClick={onClick} className={className}>
+      {label}
+    </button>
+  );
+};
