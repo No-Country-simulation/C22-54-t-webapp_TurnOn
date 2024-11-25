8df50a510c2d7021e694d6d68dc2ae8b107ba5a7
Angelo <ancricoma_angelo@hotmail.com>
2024-11-25 16:10:08 -0500

    add: add accordion

--- turnOn-front/src/App.tsx
+++ turnOn-front/src/App.tsx
@@ -1,22 +1,24 @@
-import Header from "./components/Header";
 import { Navbar } from "./components/Navbar";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
+      <main>
         <Routes>
-        <Route path="/" element={<Header/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/login" element={<Login/>}/> 
-        <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Welcome />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
-        <Footer/>    
+      </main>
+      <Footer />
     </Router>
   );
 }
