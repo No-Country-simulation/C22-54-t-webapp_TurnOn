3f8cbb53cbf1f9098f427372c77ecc992be45151
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-11-27 14:08:14 -0600

    Fix: cleaned package.json by removing duplicate entries, adjusted Tailwind CSS setup, and reorganized font imports

--- turnOn-front/src/pages/Welcome.tsx
+++ turnOn-front/src/pages/Welcome.tsx
@@ -1,12 +1,14 @@
 import Accordion from "../components/Accordion";
 import Header from "../components/Header";
 import {Clinicas} from '../components/Clinicas'
+import ShiftControl from "@/components/ShiftControl/ShiftControl";
 
 export default function Welcome() {
   return (
     <div className="bg-gray-50">
       <Header />
       <Clinicas />
+      <ShiftControl></ShiftControl>
       <Accordion />
     </div>
   );
