912be7c677bb15fb14c1df38ad5e9b669d64b101
Wkatir <110741874+wkatir@users.noreply.github.com.>
2024-12-03 20:24:41 -0600

    feat: finishing styling video section

--- turnOn-front/src/components/welcome/HowItWorks.tsx
+++ turnOn-front/src/components/welcome/HowItWorks.tsx
@@ -10,20 +10,20 @@ const HowItWorks = () => {
                 <h2 className="font-poppins font-bold text-center text-h2 md:text-h2-desktop mt-4 mb-4 md:mb-6">
                     Así funciona nuestra plataforma
                 </h2>
-                <div className="flex justify-center items-center flex-col gap-3">
+                <div className="flex justify-center items-center flex-col gap-3 relative">
                     <video
                         src="https://res.cloudinary.com/di0cvbfdb/video/upload/v1733182858/TurnOn/atfeasmabpba8e8x1f6m.mp4"
-                        loop
-                        autoPlay
-                        className="w-full md:w-[587px] md:h-[347px] rounded-2xl"
+                        controls
+                        className="w-full md:w-[587px] md:h-[347px] rounded-2xl shadow-lg"
                     />
+                    <Link to="/" className="font-roboto text-center text-p md:text-p-desktop shadow-lg bg-gradient-to-r from-white from-1% via-accent-2 via-30% to-[#9CCE3C] to-90% absolute md:bottom-36 md:left-52 left-2 py-2 px-24 rounded-2xl">DEMO</Link>
                     <p className="font-roboto text-center text-p md:text-p-desktop">
                         Descubre cómo opera nuestra plataforma desde adentro antes de registrarte.
                         Podrás explorar el proceso de programación de citas y verificar la disponibilidad
                         de servicios para la actual y la próxima semana.
                     </p>
                     <Link
-                        className="font-roboto bg-accent py-2 px-8 md:px-10 rounded-2xl text-white"
+                        className="font-roboto text-center text-p md:text-p-desktop shadow-lg bg-accent py-2 px-8 md:px-10 rounded-2xl text-white"
                         to="/"
                     >
                         Mira la demo
